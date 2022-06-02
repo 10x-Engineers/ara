01823b58a789b7d48f58a9f7dfea115029a7a765
Xiaorui Yin <yinx@student.ethz.ch>
2022-06-02 19:52:39 +0200

    add dot product

--- apps/fdotproduct/main.c
+++ apps/fdotproduct/main.c
@@ -22,7 +22,6 @@
 #include "printf.h"
 #include "runtime.h"
 
-<<<<<<< HEAD
 #include "kernel/fdotproduct.h"
 
 // Threshold for FP comparisons
@@ -68,8 +67,8 @@ extern _Float16 res16_v;
 extern double res64_s;
 extern float res32_s;
 extern _Float16 res16_s;
-=======
 // Keep vector 512 B long, to fit a vreg when LMUL == 8
+#define SIZE_8B 4096
 #define SIZE_16B 2048
 #define SIZE_32B 1024
 #define SIZE_64B 512
@@ -83,6 +82,8 @@ void dotp_32b(uint64_t avl, int32_t *v0, int32_t *v1, int32_t *result);
 void dotp_32b_scalar(uint64_t avl, int32_t *v0, int32_t *v1, int32_t *result);
 void dotp_16b(uint64_t avl, int16_t *v0, int16_t *v1, int16_t *result);
 void dotp_16b_scalar(uint64_t avl, int16_t *v0, int16_t *v1, int16_t *result);
+void dotp_8b(uint64_t avl, int8_t *v0, int8_t *v1, int8_t *result);
+void dotp_8b_scalar(uint64_t avl, int8_t *v0, int8_t *v1, int8_t *result);
 
 // Vectors for benchmarks
 int64_t v64a[SIZE_64B] __attribute__((aligned(32 * NR_LANES), section(".l2")));
@@ -91,11 +92,12 @@ int32_t v32a[SIZE_32B] __attribute__((aligned(32 * NR_LANES), section(".l2")));
 int32_t v32b[SIZE_32B] __attribute__((aligned(32 * NR_LANES), section(".l2")));
 int16_t v16a[SIZE_16B] __attribute__((aligned(32 * NR_LANES), section(".l2")));
 int16_t v16b[SIZE_16B] __attribute__((aligned(32 * NR_LANES), section(".l2")));
+int8_t v8a[SIZE_8B] __attribute__((aligned(32 * NR_LANES), section(".l2")));
+int8_t v8b[SIZE_8B] __attribute__((aligned(32 * NR_LANES), section(".l2")));
 
 int64_t v_result64, s_result64;
 int32_t v_result32, s_result32;
 int16_t v_result16, s_result16;
->>>>>>> 3ddb4810... Upload dotproduct
 
 int main() {
   printf("\n");
@@ -112,6 +114,22 @@ int main() {
     start_timer();
     res64_v = fdotp_v64b(v64a, v64b, avl);
     stop_timer();
+int8_t v_result8, s_result8;
+
+int main() {
+  printf("\n");
+  printf("==========\n");
+  printf("=  DOTP  =\n");
+  printf("==========\n");
+  printf("\n");
+  printf("\n");
+
+  int64_t runtime_s, runtime_v;
+
+  for (uint64_t avl = 8; avl <= SIZE_64B; avl *= 8) {
+    // Dotp
+    printf("Calulating 64b dotp with vectors with length = %lu\n", avl);
+    dotp_64b(avl, v64a, v64b, &v_result64);
     runtime_v = get_timer();
     printf("Vector runtime: %ld\n", runtime_v);
 
@@ -138,6 +156,13 @@ int main() {
           printf("Error: v = %f, g = %f\n", res64_v, gold64);
           return -1;
         }
+      dotp_64b_scalar(avl, v64a, v64b, &s_result64);
+      runtime_s = get_timer();
+      printf("Scalar runtime: %ld\n", runtime_s);
+
+      if (CHECK) {
+        if (v_result64 != s_result64)
+          return -1;
       }
     }
   }
@@ -147,6 +172,10 @@ int main() {
     start_timer();
     res32_v = fdotp_v32b(v32a, v32b, avl);
     stop_timer();
+  for (uint64_t avl = 16; avl <= SIZE_32B; avl *= 8) {
+    // Dotp
+    printf("Calulating 32b dotp with vectors with length = %lu\n", avl);
+    dotp_32b(avl, v32a, v32b, &v_result32);
     runtime_v = get_timer();
     printf("Vector runtime: %ld\n", runtime_v);
 
@@ -173,6 +202,13 @@ int main() {
           printf("Error: v = %f, g = %f\n", res32_v, gold32);
           return -1;
         }
+      dotp_32b_scalar(avl, v32a, v32b, &s_result32);
+      runtime_s = get_timer();
+      printf("Scalar runtime: %ld\n", runtime_s);
+
+      if (CHECK) {
+        if (v_result32 != s_result32)
+          return -1;
       }
     }
   }
@@ -183,6 +219,10 @@ int main() {
     start_timer();
     res16_v = fdotp_v16b(v16a, v16b, avl);
     stop_timer();
+  for (uint64_t avl = 32; avl <= SIZE_16B; avl *= 8) {
+    // Dotp
+    printf("Calulating 16b dotp with vectors with length = %lu\n", avl);
+    dotp_16b(avl, v16a, v16b, &v_result16);
     runtime_v = get_timer();
     printf("Vector runtime: %ld\n", runtime_v);
 
@@ -193,7 +233,6 @@ int main() {
       runtime_s = get_timer();
       printf("Scalar runtime: %ld\n", runtime_s);
     }
-<<<<<<< HEAD
 
     if (CHECK) {
       if (SCALAR) {
@@ -216,16 +255,41 @@ int main() {
         }
       }
     }
-=======
->>>>>>> 3ddb4810... Upload dotproduct
+
+      dotp_16b_scalar(avl, v16a, v16b, &s_result16);
+      runtime_s = get_timer();
+      printf("Scalar runtime: %ld\n", runtime_s);
+
+      if (CHECK) {
+        if (v_result16 != s_result16)
+          return -1;
+      }
+    }
+  }
+
+  for (uint64_t avl = 64; avl <= SIZE_8B; avl *= 8) {
+    // Dotp
+    printf("Calulating 8b dotp with vectors with length = %lu\n", avl);
+    dotp_8b(avl, v8a, v8b, &v_result8);
+    runtime_v = get_timer();
+    printf("Vector runtime: %ld\n", runtime_v);
+
+    if (SCALAR) {
+      dotp_8b_scalar(avl, v8a, v8b, &s_result8);
+      runtime_s = get_timer();
+      printf("Scalar runtime: %ld\n", runtime_s);
+
+      if (CHECK) {
+        if (v_result8 != s_result8)
+          return -1;
+      }
+    }
   }
 
   printf("SUCCESS.\n");
 
   return 0;
 }
-<<<<<<< HEAD
-=======
 
 void dotp_64b(uint64_t avl, int64_t *v0, int64_t *v1, int64_t *result) {
   uint64_t vl;
@@ -247,8 +311,8 @@ void dotp_64b(uint64_t avl, int64_t *v0, int64_t *v1, int64_t *result) {
   *(&tmp) = 0;
 
   start_timer();
-  asm volatile("vfmul.vv v24, v8, v0");
-  asm volatile("vfredsum.vs v16, v24, v16");
+  asm volatile("vmul.vv v24, v8, v0");
+  asm volatile("vredsum.vs v16, v24, v16");
   asm volatile("vsetvli %0, %1, e64, m8, ta, ma" : "=r"(tmp) : "r"(1));
   // Store the reduced value to have a memory barrier and read the actual
   // runtime
@@ -314,8 +378,8 @@ void dotp_32b(uint64_t avl, int32_t *v0, int32_t *v1, int32_t *result) {
   *(&tmp) = 0;
 
   start_timer();
-  asm volatile("vfmul.vv v24, v8, v0");
-  asm volatile("vfredsum.vs v16, v24, v16");
+  asm volatile("vmul.vv v24, v8, v0");
+  asm volatile("vredsum.vs v16, v24, v16");
   asm volatile("vsetvli %0, %1, e32, m8, ta, ma" : "=r"(tmp) : "r"(1));
   // Store the reduced value to have a memory barrier and read the actual
   // runtime
@@ -381,8 +445,8 @@ void dotp_16b(uint64_t avl, int16_t *v0, int16_t *v1, int16_t *result) {
   *(&tmp) = 0;
 
   start_timer();
-  asm volatile("vfmul.vv v24, v8, v0");
-  asm volatile("vfredsum.vs v16, v24, v16");
+  asm volatile("vmul.vv v24, v8, v0");
+  asm volatile("vredsum.vs v16, v24, v16");
   asm volatile("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(tmp) : "r"(1));
   // Store the reduced value to have a memory barrier and read the actual
   // runtime
@@ -428,4 +492,70 @@ void dotp_16b_scalar(uint64_t avl, int16_t *v0, int16_t *v1, int16_t *result) {
 
   *result = acc0;
 }
->>>>>>> 3ddb4810... Upload dotproduct
+
+void dotp_8b(uint64_t avl, int8_t *v0, int8_t *v1, int8_t *result) {
+  uint64_t vl;
+  volatile uint64_t tmp;
+
+  uint8_t zero = 0;
+
+  asm volatile("vsetvli %0, %1, e8, m8, ta, ma" : "=r"(tmp) : "r"(1));
+  asm volatile("vle8.v v0, (%0);" ::"r"(&zero));
+  asm volatile("vle8.v v8, (%0);" ::"r"(&zero));
+  asm volatile("vle8.v v16, (%0);" ::"r"(&zero));
+  asm volatile("vle8.v v24, (%0);" ::"r"(&zero));
+  asm volatile("vsetvli %0, %1, e8, m8, ta, ma" : "=r"(vl) : "r"(avl));
+  asm volatile("vle8.v v8, (%0);" ::"r"(v0));
+  asm volatile("vle8.v v0, (%0);" ::"r"(v1));
+
+  // Force the load to be over
+  *(&tmp) = 0;
+
+  start_timer();
+  asm volatile("vmul.vv v24, v8, v0");
+  asm volatile("vredsum.vs v8, v24, v8");
+  asm volatile("vsetvli %0, %1, e8, m8, ta, ma" : "=r"(tmp) : "r"(1));
+  // Store the reduced value to have a memory barrier and read the actual
+  // runtime
+  asm volatile("vse8.v v8, (%0);" ::"r"(result));
+  stop_timer();
+}
+
+void dotp_8b_scalar(uint64_t avl, int8_t *v0, int8_t *v1, int8_t *result) {
+  int8_t acc0, acc1, acc2, acc3, acc4, acc5, acc6, acc7;
+
+  acc0 = 0;
+  acc1 = 0;
+  acc2 = 0;
+  acc3 = 0;
+  acc4 = 0;
+  acc5 = 0;
+  acc6 = 0;
+  acc7 = 0;
+  // Start timer
+  start_timer();
+  for (uint64_t i = 0; i < avl; i += 8) {
+    acc0 += v0[i + 0] * v1[i + 0];
+    acc1 += v0[i + 1] * v1[i + 1];
+    acc2 += v0[i + 2] * v1[i + 2];
+    acc3 += v0[i + 3] * v1[i + 3];
+    acc4 += v0[i + 4] * v1[i + 4];
+    acc5 += v0[i + 5] * v1[i + 5];
+    acc6 += v0[i + 6] * v1[i + 6];
+    acc7 += v0[i + 7] * v1[i + 7];
+  }
+
+  acc0 += acc1;
+  acc2 += acc3;
+  acc4 += acc5;
+  acc6 += acc7;
+
+  acc0 += acc2;
+  acc4 += acc6;
+
+  acc0 += acc4;
+  // Stop timer
+  stop_timer();
+
+  *result = acc0;
+}
