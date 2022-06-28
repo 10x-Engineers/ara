da614a8b09a0d934691a0b2cb64054eb2e282002
M-Ijaz-10x <muhammad.ijaz@10xengineers.ai>
2022-06-28 17:29:58 +0500

    Fixed the issues with vssub.c fixed-point test

--- apps/riscv-tests/isa/rv64uv/vssub.c
+++ apps/riscv-tests/isa/rv64uv/vssub.c
@@ -13,7 +13,7 @@ void TEST_CASE1(void) {
   VLOAD_32(v1, 0xfffffff0, 0x7FFFFFFC, 15, 20);
   VLOAD_32(v2, 0x7ffffff0, -500, 3, 25);
   __asm__ volatile("vssub.vv v3, v1, v2" ::);
-  VCMP_U32(1, v3, 0x80000000, 0x7fffffff, 12, -5);
+  VCMP_U32(1, v3, 0x80000000, 0, 12, 0);
   read_vxsat(vxsat)
   check_vxsat(1, vxsat, 1);
 }
@@ -26,7 +26,7 @@ void TEST_CASE2(void) {
   VLOAD_32(v0, 10, 0, 0, 0);
   VCLEAR(v3);
   __asm__ volatile("vssub.vv v3, v1, v2, v0.t" ::);
-  VCMP_U32(1, v3, 0, 0x7fffffff, 0, -5);
+  VCMP_U32(1, v3, 0, 0, 0, 0);
   read_vxsat(vxsat)
   check_vxsat(2, vxsat, 1);
 }
@@ -37,9 +37,9 @@ void TEST_CASE3(void) {
   VLOAD_32(v1, 5, -2147483645, 15, 20);
   const int64_t scalar = 5;
   __asm__ volatile("vssub.vx v3, v1, %[A]" ::[A] "r"(scalar));
-  VCMP_U32(3, v3, 0, 0x80000000, 10, 15);
+  VCMP_U32(3, v3, 0, 0x7FFFFFFE, 10, 15);
   read_vxsat(vxsat)
-  check_vxsat(3, vxsat, 1);
+  check_vxsat(3, vxsat, 0);
 }
 
 void TEST_CASE4(void) {
@@ -49,7 +49,7 @@ void TEST_CASE4(void) {
   VLOAD_32(v0, 10, 0, 0, 0);
   VCLEAR(v3);
   __asm__ volatile("vssub.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
-  VCMP_U32(4, v3, 0, 0x80000000, 0, 15);
+  VCMP_U32(4, v3, 0, 0x7FFFFFFE, 0, 15);
 }
 
 int main(void) {
