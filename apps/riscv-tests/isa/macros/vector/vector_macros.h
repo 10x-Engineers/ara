6b3fdfdb0e1a54000a019b91330c8f7700ac7b8f
quswar-abid <quswar.abid@10xengineers.ai>
2022-06-29 13:46:56 +0500

    added two tests (vxsat & vxrm), macro functions used by them in vector_macros.h and also included them in Makefrag

--- apps/riscv-tests/isa/macros/vector/vector_macros.h
+++ apps/riscv-tests/isa/macros/vector/vector_macros.h
@@ -45,6 +45,7 @@ int test_case;
 #define read_mstatus(buf) do { asm volatile ("csrr %[BUF], mstatus" : [BUF] "=r" (buf)); } while (0);
 #define read_misa(buf) do { asm volatile ("csrr %[BUF], misa" : [BUF] "=r" (buf)); } while (0);
 #define read_vxsat(buf) do { asm volatile ("csrr %[BUF], vxsat" : [BUF] "=r" (buf)); } while (0);
+#define read_vxrm(buf) do { asm volatile ("csrr %[BUF], vxrm" : [BUF] "=r" (buf)); } while (0);
 #define set_vxrm(val) do { asm volatile ("csrw vxrm, %0" :: "rK"(val)); } while (0);
 
 #define vtype(golden_vtype, vlmul, vsew, vta, vma) (golden_vtype = vlmul << 0 | vsew << 3 | vta << 6 | vma << 7)
@@ -80,6 +81,15 @@ int test_case;
   printf("Checking vxsat #%d...\n", casenum);                                                                      \
   if (vxsat != golden_vxsat) {                                                                                     \
     printf("FAILED. Got vxsat = %lx, expected vxsat = %lx.\n", vxsat, golden_vxsat);                               \
+    num_failed++;                                                                                                  \
+    return;                                                                                                        \
+  }                                                                                                                \
+  printf("PASSED.\n");
+
+#define check_vxrm(casenum, vxrm, golden_vxrm)                                                                     \
+  printf("Checking vxrm #%d...\n", casenum);                                                                       \
+  if (vxrm != golden_vxrm) {                                                                                       \
+    printf("FAILED. Got vxrm = %lx, expected vxrm = %lx.\n", vxrm, golden_vxrm);                                   \
     num_failed++;                                                                                                  \
     return;                                                                                                        \
   }                                                                                                                \
---
+++ apps/riscv-tests/isa/rv64uv/vxrm.c
@@ -0,0 +1,131 @@
+// Copyright 2021 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+//
+// Author: Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
+//         Basile Bougenot <bbougenot@student.ethz.ch>
+
+#include "vector_macros.h"
+
+void TEST_CASE1(void) {
+  uint64_t vxrm;
+  set_vxrm(0); // setting vxrm to rnu rounding mode
+  VSET(4, e8, m1);
+  VLOAD_8(v1, 1, -2, -3, 4);
+  VLOAD_8(v2, 1, 2, -3, 3);
+  __asm__ volatile("vaadd.vv v3, v1, v2" ::);
+  VCMP_U8(1, v3, 1, 0, -3, 4);
+  read_vxrm(vxrm);
+  check_vxrm(1, vxrm >> 2, 0);// The upper bits, vxrm[XLEN-1:2], should be written as zeros
+}
+
+void TEST_CASE2(void) {
+  uint64_t vxrm;
+  set_vxrm(1); // setting vxrm to rne rounding mode
+  VSET(4, e8, m1);
+  VLOAD_8(v1, 1, -2, -3, 4);
+  VLOAD_8(v2, 1, 9, -3, 5);
+  VLOAD_8(v0, 0xA, 0x0, 0x0, 0x0);
+  VCLEAR(v3);
+  __asm__ volatile("vaadd.vv v3, v1, v2, v0.t" ::);
+  VCMP_U8(2, v3, 0, 4, 0, 4);
+  read_vxrm(vxrm);
+  check_vxrm(2, vxrm >> 2, 0);// The upper bits, vxrm[XLEN-1:2], should be written as zeros
+}
+
+void TEST_CASE3(void) {
+  uint64_t vxrm;
+  set_vxrm(2); // setting vxrm to rdn rounding mode
+  VSET(4, e32, m1);
+  VLOAD_32(v1, 1, -2, 3, -4);
+  const uint32_t scalar = 5;
+  __asm__ volatile("vaadd.vx v3, v1, %[A]" ::[A] "r"(scalar));
+  VCMP_U32(3, v3, 3, 1, 4, 0);
+  read_vxrm(vxrm);
+  check_vxrm(3, vxrm >> 2, 0);// The upper bits, vxrm[XLEN-1:2], should be written as zeros
+}
+
+// Dont use VCLEAR here, it results in a glitch where are values are off by 1
+void TEST_CASE4(void) {
+  uint64_t vxrm;
+  set_vxrm(3); // setting vxrm to rod rounding mode
+  VSET(4, e32, m1);
+  VLOAD_32(v1, 1, 2, 3, 4);
+  const uint32_t scalar = 5;
+  VLOAD_32(v0, 0xA, 0x0, 0x0, 0x0);
+  VCLEAR(v3);
+  __asm__ volatile("vaadd.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
+  VCMP_U32(4, v3, 0, 3, 0, 5);
+  read_vxrm(vxrm);
+  check_vxrm(4, vxrm >> 2, 0);// The upper bits, vxrm[XLEN-1:2], should be written as zeros
+}
+
+
+void TEST_CASE5(void) {
+  uint64_t vxrm;
+  set_vxrm(0); // setting vxrm to rnu rounding mode
+  VSET(4, e8, m1);
+  VLOAD_8(v1, 1, 2, 3, 5);
+  VLOAD_8(v2, 1, 3, 8, 4);
+  __asm__ volatile("vaaddu.vv v3, v1, v2" ::);
+  VCMP_U8(5, v3, 1, 3, 6, 5);
+  read_vxrm(vxrm);
+  check_vxrm(5, vxrm >> 2, 0);// The upper bits, vxrm[XLEN-1:2], should be written as zeros
+}
+
+void TEST_CASE6(void) {
+  uint64_t vxrm;
+  set_vxrm(1); // setting vxrm to rne rounding mode
+  VSET(4, e8, m1);
+  VLOAD_8(v1, 5, 8, 3, 7);
+  VLOAD_8(v2, 7, 5, 3, 5);
+  VLOAD_8(v0, 0x0A, 0x00, 0x00, 0x00);
+  VCLEAR(v3);
+  __asm__ volatile("vaaddu.vv v3, v1, v2, v0.t" ::);
+  VCMP_U8(6, v3, 0, 6, 0, 6);
+  read_vxrm(vxrm);
+  check_vxrm(6, vxrm >> 2, 0);// The upper bits, vxrm[XLEN-1:2], should be written as zeros
+}
+
+void TEST_CASE7(void) {
+  uint64_t vxrm;
+  set_vxrm(2); // setting vxrm to rdn rounding mode
+  VSET(4, e32, m1);
+  VLOAD_32(v1, 1, 2, 3, 4);
+  const uint32_t scalar = 5;
+  __asm__ volatile("vaaddu.vx v3, v1, %[A]" ::[A] "r"(scalar));
+  VCMP_U32(7, v3, 3, 3, 4, 4);
+  read_vxrm(vxrm);
+  check_vxrm(7, vxrm >> 2, 0);// The upper bits, vxrm[XLEN-1:2], should be written as zeros
+}
+
+// Dont use VCLEAR here, it results in a glitch where are values are off by 1
+void TEST_CASE8(void) {
+  uint64_t vxrm;
+  set_vxrm(3); // setting vxrm to rod rounding mode
+  VSET(4, e32, m1);
+  VLOAD_32(v1, 1, 2, 3, 4);
+  const uint32_t scalar = 5;
+  VLOAD_32(v0, 0xA, 0x0, 0x0, 0x0);
+  VCLEAR(v3);
+  __asm__ volatile("vaaddu.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
+  VCMP_U32(8, v3, 0, 3, 0, 5);
+  read_vxrm(vxrm);
+  check_vxrm(8, vxrm >> 2, 0);// The upper bits, vxrm[XLEN-1:2], should be written as zeros
+}
+
+int main(void) {
+  INIT_CHECK();
+  enable_vec();
+  //vaadd.vv based cases
+  TEST_CASE1();
+  TEST_CASE2();
+  TEST_CASE3();
+  TEST_CASE4();
+  //vaaddu.vv based cases
+  TEST_CASE5();
+  TEST_CASE6();
+  TEST_CASE7();
+  TEST_CASE8();
+  EXIT_CHECK();
+}
