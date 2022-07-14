df68ea73af38589a42b5847c0bbf83f0d236bfea
Muhammad Ijaz <muhammad.ijaz@10xengineers.ai>
2022-07-14 12:08:01 +0500

    misa.c and mstatus_vs_sd.c test addition (#5)
    * Updated URL and hash to clone cva6 submodule
    
    * Added misa.c and mstatus_vs_sd.c tests
    
    * whitespace trim
    
    * Updated syntax of misa.c and mstatus_vs_sd.c tests

---
+++ apps/riscv-tests/isa/rv64uv/misa.c
@@ -0,0 +1,54 @@
+// Copyright 2021 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+//
+// Author: Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
+//         Basile Bougenot <bbougenot@student.ethz.ch>
+
+#include "vector_macros.h"
+
+char str_1[] = "V";
+
+void TEST_CASE1(void) {
+  uint64_t misa = -1;
+  uint64_t misa_var = 0;
+  VSET(16, e8, m1);
+  VLOAD_8(v1, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8);
+  VLOAD_8(v2, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8);
+  asm volatile("vadd.vv v3, v1, v2");
+  VCMP_U8(1, v3, 2, 4, 6, 8, 10, 12, 14, 16, 2, 4, 6, 8, 10, 12, 14, 16);
+  asm volatile("ecall");
+  read_misa(misa);
+  misa_var = misa >> 21;
+  check_misa(str_1, misa_var & 1, 1);
+}
+
+// Exception Handler
+
+void mtvec_handler(void) {
+
+  // Read mcause
+  asm volatile("csrr t0, mcause");
+
+  // Read mepc
+  asm volatile("csrr t1, mepc");
+
+  // Increment return address by 4
+  asm volatile("addi t1, t1, 4");
+  asm volatile("csrw mepc, t1");
+
+  // Filter with mcause and handle here
+
+  asm volatile("mret");
+}
+
+// Main
+
+int main(void) {
+  INIT_CHECK();
+  enable_vec();
+
+  TEST_CASE1();
+
+  EXIT_CHECK();
+}
