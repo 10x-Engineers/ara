41630727e57e99410bad46d00975d4c99f3af33e
Nouman <nouman.akbar@10xengineers.ai>
2022-07-15 13:12:36 +0500

    Fixed vxsat test failing on spike

--- apps/riscv-tests/isa/rv64uv/vxsat.c
+++ apps/riscv-tests/isa/rv64uv/vxsat.c
@@ -12,6 +12,7 @@ void TEST_CASE1(void) {
   VSET(4, e8, m1);
   VLOAD_8(v1, -80, 2, 100, 4);
   VLOAD_8(v2, -90, 2, 50, 4);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsadd.vv v3, v1, v2" ::);
   VCMP_U8(1, v3, 0x80, 4, 127, 8);
   read_vxsat(vxsat);
@@ -27,6 +28,7 @@ void TEST_CASE2(void) {
   VLOAD_8(v2, -90, 2, 50, 4);
   VLOAD_8(v0, 0xA, 0x0, 0x0, 0x0);
   VCLEAR(v3);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsadd.vv v3, v1, v2, v0.t" ::);
   VCMP_U8(2, v3, 0, 4, 0, 8);
   read_vxsat(vxsat);
@@ -39,6 +41,7 @@ void TEST_CASE3(void) {
   uint64_t vxsat = -1;
   VSET(4, e32, m1);
   VLOAD_32(v1, 1, 0x7FFFFFFB, 3, 4);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsadd.vi v3, v1, 5" ::);
   VCMP_U32(3, v3, 6, 0x7FFFFFFF, 8, 9);
   read_vxsat(vxsat);
@@ -54,6 +57,7 @@ void TEST_CASE4(void) {
   VLOAD_32(v1, 1, 0x7FFFFFFC, 0xFFFFFFFD, 2);
   VLOAD_32(v0, 0xA, 0x0, 0x0, 0x0);
   VCLEAR(v3);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsadd.vi v3, v1, 5, v0.t" ::);
   VCMP_U32(4, v3, 0, 0x7FFFFFFF, 0, 7);
   read_vxsat(vxsat);
@@ -67,6 +71,7 @@ void TEST_CASE5(void) {
   VSET(4, e32, m1);
   VLOAD_32(v1, 0x7FFFFFFD, 2, 3, 4);
   const uint32_t scalar = 5;
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsadd.vx v3, v1, %[A]" ::[A] "r"(scalar));
   VCMP_U32(5, v3, 0x7FFFFFFF, 7, 8, 9);
   read_vxsat(vxsat);
@@ -83,6 +88,7 @@ void TEST_CASE6(void) {
   const uint32_t scalar = 5;
   VLOAD_32(v0, 0xA, 0x0, 0x0, 0x0);
   VCLEAR(v3);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsadd.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
   VCMP_U32(6, v3, 0, 0x7FFFFFFF, 0, 9);
   read_vxsat(vxsat);
@@ -96,6 +102,7 @@ void TEST_CASE7(void) {
   VSET(4, e8, m1);
   VLOAD_8(v1, 133, 2, 220, 4);
   VLOAD_8(v2, 133, 2, 50, 4);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsaddu.vv v3, v1, v2" ::);
   VCMP_U8(7, v3, 255, 4, 255, 8);
   read_vxsat(vxsat);
@@ -111,6 +118,7 @@ void TEST_CASE8(void) {
   VLOAD_8(v2, 1, 124, 3, 2);
   VLOAD_8(v0, 0xA, 0x0, 0x0, 0x0);
   VCLEAR(v3);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsaddu.vv v3, v1, v2, v0.t" ::);
   VCMP_U8(8, v3, 0, 255, 0, 4);
   read_vxsat(vxsat);
@@ -123,6 +131,7 @@ void TEST_CASE9(void) {
   uint64_t vxsat = -1;
   VSET(4, e32, m1);
   VLOAD_32(v1, 1, 0xFFFFFFFB, 3, 4);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsaddu.vi v3, v1, 5" ::);
   VCMP_U32(9, v3, 6, 0xFFFFFFFF, 8, 9);
   read_vxsat(vxsat);
@@ -138,6 +147,7 @@ void TEST_CASE10(void) {
   VLOAD_32(v1, 1, 0xFFFFFFFC, 0xFFFFFFFD, 2);
   VLOAD_32(v0, 0xA, 0x0, 0x0, 0x0);
   VCLEAR(v3);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsaddu.vi v3, v1, 5, v0.t" ::);
   VCMP_U32(10, v3, 0, 0xFFFFFFFF, 0, 7);
   read_vxsat(vxsat);
@@ -151,6 +161,7 @@ void TEST_CASE11(void) {
   VSET(4, e32, m1);
   VLOAD_32(v1, 0xFFFFFFFD, 2, 3, 4);
   const uint32_t scalar = 5;
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsaddu.vx v3, v1, %[A]" ::[A] "r"(scalar));
   VCMP_U32(11, v3, 0xFFFFFFFF, 7, 8, 9);
   read_vxsat(vxsat);
@@ -167,6 +178,7 @@ void TEST_CASE12(void) {
   const uint32_t scalar = 5;
   VLOAD_32(v0, 0xA, 0x0, 0x0, 0x0);
   VCLEAR(v3);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsaddu.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
   VCMP_U32(12, v3, 0, 0xFFFFFFFF, 0, 9);
   read_vxsat(vxsat);
@@ -181,6 +193,7 @@ void TEST_CASE13(void) {
   VLOAD_32(v1, 1, 0x0000FFFF, 3, 4);
   VLOAD_32(v2, 0xA, 0xFFFF0000, 0x0, 0x0);
   VCLEAR(v3);
+  asm volatile("csrw vxsat, x0");
   __asm__ volatile("vsaddu.vv v3, v1, v2" ::);
   VCMP_U32(13, v3, 0xB, 0xFFFFFFFF, 3, 4);
   read_vxsat(vxsat);
