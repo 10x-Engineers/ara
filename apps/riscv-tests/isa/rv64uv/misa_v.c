0c6c2c29db085142b5ba9bdc7f9fe0650b25c139
Muhammad Ijaz <muhammad.ijaz@10xengineers.ai>
2022-07-15 11:46:13 +0500

    Renamed misa_v.c and mstatus_vs.c tests and added them to regression (#7)
    * Added functions for misa and mstatus and added them in regression list
    
    * Added trap handling routine for spike in mstatus_vs and misa_v tests
    
    Co-authored-by: Nouman <nouman.akbar@10xengineers.ai>

--- apps/riscv-tests/isa/rv64uv/misa_v.c
+++ apps/riscv-tests/isa/rv64uv/misa_v.c
@@ -23,9 +23,28 @@ void TEST_CASE1(void) {
   check_misa(str_1, misa_var & 1, 1);
 }
 
-// Exception Handler
+// Exception Handler for rtl
 
 void mtvec_handler(void) {
+
+  // Read mcause
+  asm volatile("csrr t0, mcause");
+
+  // Read mepc
+  asm volatile("csrr t1, mepc");
+
+  // Increment return address by 4
+  asm volatile("addi t1, t1, 4");
+  asm volatile("csrw mepc, t1");
+
+  // Filter with mcause and handle here
+
+  asm volatile("mret");
+}
+
+// Exception Handler for Spike
+
+void handle_trap(void) {
 
   // Read mcause
   asm volatile("csrr t0, mcause");
