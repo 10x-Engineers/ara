bf0518af98b270b5ae09753501a3ec85a664b17f
Muhammad Ijaz <muhammad.ijaz@10xengineers.ai>
2022-07-25 11:04:50 +0500

    updated input data width for vfirst instruction

--- hardware/src/masku/masku.sv
+++ hardware/src/masku/masku.sv
@@ -1004,7 +1004,7 @@ module masku import ara_pkg::*; import rvv_pkg::*; #(
 
 // vfirst implementation
   lzc #(
-    .WIDTH   (DataWidth),
+    .WIDTH   (NrLanes*DataWidth),
     .MODE    (1)
   ) i_clz_64b (
     .in_i    (vfirst_to_count),
