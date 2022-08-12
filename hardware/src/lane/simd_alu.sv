6abfef9da81537259993b9060ec8a24f0174ce63
M-Ijaz-10x <muhammad.ijaz@10xengineers.ai>
2022-08-12 13:14:11 +0500

    Fixes to simd_alu and masku

--- hardware/src/lane/simd_alu.sv
+++ hardware/src/lane/simd_alu.sv
@@ -138,6 +138,7 @@ module simd_alu import ara_pkg::*; import rvv_pkg::*; #(
                 end
             end
         end
+
         // Arithmetic instructions
         VSADDU: unique case (vew_i)
             EW8: for (int b = 0; b < 8; b++) begin
