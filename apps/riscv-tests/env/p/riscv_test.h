af55778d9a9ef698c0bf1021871ecd05923a1089
quswar-abid <quswar.abid@10xengineers.ai>
2022-08-30 18:35:54 +0500

    Overridden the machine mode in all trap vectors (user/0 to machine/3) and it still didn't helped not getting trapped at illegal instruction i.e. csrs fcsr, 0

--- apps/riscv-tests/env/p/riscv_test.h
+++ apps/riscv-tests/env/p/riscv_test.h
@@ -212,7 +212,7 @@ reset_vector:                                                           \
                (1 << CAUSE_USER_ECALL) |                                \
                (1 << CAUSE_BREAKPOINT);                                 \
         csrw medeleg, t0;                                               \
-1:      csrwi mstatus, 0;                                               \
+1:      csrwi mstatus, 3;                                               \
         init;                                                           \
         EXTRA_INIT;                                                     \
         EXTRA_INIT_TIMER;                                               \
