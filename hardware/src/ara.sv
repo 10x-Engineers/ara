2485a856280e646d93e63e5482099c336e9345fc
M-Ijaz-10x <muhammad.ijaz@10xengineers.ai>
2022-09-02 10:51:36 +0500

    Add support for viota, vid to go with any number of elements/lanes

--- hardware/src/ara.sv
+++ hardware/src/ara.sv
@@ -206,6 +206,18 @@ module ara import ara_pkg::*; #(
   logic                                        addrgen_operand_ready;
   logic      [NrLanes-1:0]                     sldu_red_valid;
 
+  // Mask unit operands
+  elen_t     [NrLanes-1:0][NrMaskFUnits+2-1:0] masku_operand;
+  logic      [NrLanes-1:0][NrMaskFUnits+2-1:0] masku_operand_valid;
+  logic      [NrLanes-1:0][NrMaskFUnits+2-1:0] masku_operand_ready;
+  strb_t     [NrLanes-1:0]                     mask;
+  logic      [NrLanes-1:0]                     mask_valid;
+  logic                                        mask_valid_lane;
+  logic      [NrLanes-1:0]                     lane_mask_ready;
+  elen_t     [NrLanes-1:0]                     alu_operand;
+  logic      [NrLanes-1:0]                     alu_operand_valid;
+  elen_t     [NrLanes-1:0]                     viota_operand;
+  logic      [NrLanes-1:0]                     viota_operand_valid;
   // Results
   // Load Unit
   logic      [NrLanes-1:0]                     ldu_result_req;
@@ -231,9 +243,6 @@ module ara import ara_pkg::*; #(
   strb_t     [NrLanes-1:0]                     masku_result_be;
   logic      [NrLanes-1:0]                     masku_result_gnt;
   logic      [NrLanes-1:0]                     masku_result_final_gnt;
-  elen_t     [NrLanes-1:0]                     alu_operand;
-  elen_t     [NrLanes-1:0]                     alu_operand_combined;
-  logic      [NrLanes-1:0]                     alu_operand_valid;
 
   for (genvar lane = 0; lane < NrLanes; lane++) begin: gen_lanes
     lane #(
@@ -246,9 +255,6 @@ module ara import ara_pkg::*; #(
       .scan_data_i                     (1'b0                                ),
       .scan_data_o                     (/* Unused */                        ),
       .lane_id_i                       (lane[idx_width(NrLanes)-1:0]        ),
-      // Mask instructions
-      .alu_operand_o                   (alu_operand[lane]                   ),
-      .alu_operand_valid_o             (alu_operand_valid[lane]             ),
       // Interface with the dispatcher
       .vxsat_flag_o                    (vxsat_flag[lane]                    ),
       .alu_vxrm_i                      (alu_vxrm[lane]                      ),
@@ -304,7 +310,11 @@ module ara import ara_pkg::*; #(
       .masku_result_final_gnt_o        (masku_result_final_gnt[lane]        ),
       .mask_i                          (mask[lane]                          ),
       .mask_valid_i                    (mask_valid[lane] & mask_valid_lane  ),
-      .mask_ready_o                    (lane_mask_ready[lane]               )
+      .mask_ready_o                    (lane_mask_ready[lane]               ),
+      .alu_operand_o                   (alu_operand[lane]                   ),
+      .alu_operand_valid_o             (alu_operand_valid[lane]             ),
+      .viota_operand_o                 (viota_operand[lane]                 ),
+      .viota_operand_valid_o           (viota_operand_valid[lane]           )
     );
   end: gen_lanes
 
@@ -417,18 +427,12 @@ module ara import ara_pkg::*; #(
   //  Mask unit  //
   /////////////////
 
-  for (genvar lane=0; lane<NrLanes; lane++) begin
-      assign alu_operand_combined [lane] = alu_operand [lane];
-  end
-
   masku #(
     .NrLanes(NrLanes),
     .vaddr_t(vaddr_t)
   ) i_masku (
     .clk_i                   (clk_i                           ),
     .rst_ni                  (rst_ni                          ),
-    .alu_operand_i           (alu_operand                     ),
-    .alu_operand_valid_i     (alu_operand_valid               ),
     // Interface with the main sequencer
     .pe_req_i                (pe_req                          ),
     .pe_req_valid_i          (pe_req_valid                    ),
@@ -448,6 +452,10 @@ module ara import ara_pkg::*; #(
     .masku_result_be_o       (masku_result_be                 ),
     .masku_result_gnt_i      (masku_result_gnt                ),
     .masku_result_final_gnt_i(masku_result_final_gnt          ),
+    .alu_operand_i           (alu_operand                     ),
+    .alu_operand_valid_i     (alu_operand_valid               ),
+    .viota_operand_i         (viota_operand                   ),
+    .viota_operand_valid_i   (viota_operand_valid             ),
     // Interface with the VFUs
     .mask_o                  (mask                            ),
     .mask_valid_o            (mask_valid                      ),
