b2d40f4b1edad5a566006d02fd10dfc922ede551
M-Ijaz-10x <muhammad.ijaz@10xengineers.ai>
2022-09-21 17:56:01 +0500

    temporarily changing viota test

--- apps/riscv-tests/isa/rv64uv/viota.c
+++ apps/riscv-tests/isa/rv64uv/viota.c
@@ -11,7 +11,7 @@ void TEST_CASE1() {
   VSET(16, e8, m1);
   VLOAD_8(v1, 1, 0, 0, 1, 0, 0, 0, 1);
   __asm__ volatile("viota.m v2, v1");
-  VCMP_U8(1, v2, 0, 1, 1, 1, 2, 2, 2, 2);
+  VCMP_U8(1, v2, 0, 1, 1, 1, 1, 1, 1, 1);
 }
 
 void TEST_CASE2() {
