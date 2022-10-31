2007d824b4766c9b0874ad7fcfb531dec095ef3e
quswar-abid <quswar.abid@10xengineers.ai>
2022-10-31 10:20:08 +0500

    one test case

--- apps/riscv-tests/isa/rv64uv/vadd.c
+++ apps/riscv-tests/isa/rv64uv/vadd.c
@@ -7,12 +7,13 @@
 
 #include "vector_macros.h"
 
+
 void TEST_CASE(void) {
-  VSET(10, e8, m2);
+  VSET(8, e8, m2);
   VLOAD_8(v0, 1, 2, 3, 4, 5, 6, 7, 8);
   VLOAD_8(v2, 1, 2, 3, 4, 5, 6, 7, 8);
   asm volatile("vadd.vv v4, v0, v2");
-  VCMP_U8(0, v4, 2, 4, 6, 8, 10, 12, 14, 16);
+  VCMP_U8(1, v4, 2, 4, 6, 8, 10, 12, 14, 16);
 }
 
 
